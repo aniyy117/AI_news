b80abc4c66ac945409e26b62f69fc7e3c2eb13f0
aniyy117 <janiket112@gmail.com>
2022-02-07 19:47:13 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Typography } from '@material-ui/core';
-import wordsToNumbers from 'words-to-numbers';
-import alanBtn from '@alan-ai/alan-sdk-web';
+import React, { useState, useEffect } from "react";
+import { Typography } from "@material-ui/core";
+import wordsToNumbers from "words-to-numbers";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-import NewsCards  from './components/NewsCards/NewsCards';
-import useStyles from './styles';
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
 
 const App = () => {
   const [activeArticle, setActiveArticle] = useState(0);
@@ -15,26 +15,28 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: '934f6b56adf473727e332c1e3d8f9add2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      key: "934f6b56adf473727e332c1e3d8f9add2e956eca572e1d8b807a3e2338fdd0dc/stage",
       onCommand: ({ command, articles, number }) => {
-        if (command === 'newHeadlines') {
+        if (command === "newHeadlines") {
           setNewsArticles(articles);
           setActiveArticle(-1);
-        } else if (command === 'instructions') {
-          
-        } else if (command === 'highlight') {
+        } else if (command === "instructions") {
+        } else if (command === "highlight") {
           setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
-        } else if (command === 'open') {
-          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
           const article = articles[parsedNumber - 1];
 
           if (parsedNumber > articles.length) {
-            alanBtn().playText('Please try that again...');
+            alanBtn().playText("Please try that again...");
           } else if (article) {
-            window.open(article.url, '_blank');
-            alanBtn().playText('Opening...');
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
           } else {
-            alanBtn().playText('Please try that again...');
+            alanBtn().playText("Please try that again...");
           }
         }
       },
@@ -46,11 +48,27 @@ const App = () => {
       <div className={classes.logoContainer}>
         {newsArticles.length ? (
           <div className={classes.infoContainer}>
-            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
-            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
           </div>
         ) : null}
-        <img src="https://i2.wp.com/synqqblog.wpcomstaging.com/wp-content/uploads/2019/07/screen-shot-2019-06-06-at-10.15.45-am.png?fit=400%2C136&ssl=1" className={classes.alanLogo} alt="logo" />
+        <img
+          src="https://i2.wp.com/synqqblog.wpcomstaging.com/wp-content/uploads/2019/07/screen-shot-2019-06-06-at-10.15.45-am.png?fit=400%2C136&ssl=1"
+          className={classes.alanLogo}
+          alt="logo"
+        />
       </div>
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
